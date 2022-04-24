ebc0ce2a40b0239a7d7ce16593f936c98b908e9a
apekshaalaad <102643138+apekshaalaad@users.noreply.github.com>
2022-04-24 21:03:22 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,76 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 
-namespace CsharpAssign5
+namespace CsharpAssign8
 {
+    class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public double EmpSalary { get; set; }
+
+        public Employee(int EmpId, string EmpName, double EmpSalary)
+        {
+            this.EmpId = EmpId;
+            this.EmpName = EmpName;
+            this.EmpSalary = EmpSalary;
+        }
+        public void empDetails()
+        {
+            Console.WriteLine("Employee Id :" + EmpId);
+            Console.WriteLine("Employee name :" + EmpName);
+            Console.WriteLine("Employee Salary :" + EmpSalary);
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            int[] arr = new int[5];
-            int i, j;
-            int[] arr1 = new int[5];
-            Console.Write("Input 5 elements in the 1st array :\n");
-            for (i = 0; i < 5; i++)
+            Employee emp = new Employee(1, "Apeksha", 60000);
+
+            Assembly executing = Assembly.GetExecutingAssembly();
+            Type[] types = executing.GetTypes();
+            emp.empDetails();
+            foreach (var item in types)
             {
-                Console.Write(" ", i);
-                arr[i] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(item.Name);
+                MethodInfo[] mi = item.GetMethods();
+
+                foreach (var method in mi)
+                {
+                    Console.WriteLine(method.Name);
+
+                }
             }
-
-            Console.Write("\nElements in 1st array are: ");
-            for (i = 0; i < 5; i++)
-            {
-                Console.Write("{0}  ", arr[i]);
-            }
-            Console.Write("\nInput 5 elements in the 2nd array :\n");
-            for (j = 0; j < 5; j++)
-            {
-                Console.Write(" ", i);
-                arr1[j] = Convert.ToInt32(Console.ReadLine());
-            }
-
-            Console.Write("\nElements in 2nd array are: ");
-            for (j = 0; j < 5; j++)
-            {
-                Console.Write("{0}  ", arr1[j]);
-            }
-            System.Array.Sort(arr);
-            System.Array.Sort(arr1);
-            foreach (int value in arr)
-            {
-                Console.Write("\nSorted 1st Array " + value);
-            }
-            foreach (int value1 in arr1)
-            {
-                Console.Write("\nSorted 2nd Array " + value1);
-            }
-            System.Array.Copy(arr, 0, arr1, 0, 5);
-            Console.WriteLine("\nCopied 2nd Array: ");
-            foreach (int res in arr1)
-            {
-                Console.WriteLine(res);
-            }
-            System.Array.Reverse(arr);
-
-            Console.WriteLine(String.Join("\nReversed 1st Array: ", arr));
-            Array.Clear(arr, 0, arr.Length);
-            Console.WriteLine("\n1st Array (After using Clear): ");
-            foreach (int val in arr)
-            {
-                Console.WriteLine(val);
-            }
-            Console.Write("\n");
-
-
-
-
-
-           
-
-            Console.ReadKey();
+            Console.ReadLine();
         }
     }
 }
