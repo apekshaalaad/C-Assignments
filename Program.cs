80fc5923584d4e54fcaddec924f7b9680e410ffe
apekshaalaad <102643138+apekshaalaad@users.noreply.github.com>
2022-04-24 21:04:12 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,48 +1,71 @@
 ﻿using System;
-using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace CsharpAssign8
+namespace CsharpAssign7
 {
-    class Employee
-    {
-        public int EmpId { get; set; }
-        public string EmpName { get; set; }
-        public double EmpSalary { get; set; }
-
-        public Employee(int EmpId, string EmpName, double EmpSalary)
-        {
-            this.EmpId = EmpId;
-            this.EmpName = EmpName;
-            this.EmpSalary = EmpSalary;
-        }
-        public void empDetails()
-        {
-            Console.WriteLine("Employee Id :" + EmpId);
-            Console.WriteLine("Employee name :" + EmpName);
-            Console.WriteLine("Employee Salary :" + EmpSalary);
-        }
-    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Employee emp = new Employee(1, "Apeksha", 60000);
+            string text = System.IO.File.ReadAllText(@"C:\Users\user\source\repos\2nd csharp.txt");
 
-            Assembly executing = Assembly.GetExecutingAssembly();
-            Type[] types = executing.GetTypes();
-            emp.empDetails();
-            foreach (var item in types)
+            // Display the file contents to the console. Variable text is a string.
+            System.Console.WriteLine("Contents of 2nd csharp.txt = {0}", text);
+
+            System.IO.DriveInfo di = new System.IO.DriveInfo(@"C:\Users\user\source\repos");
+            Console.WriteLine(di.TotalFreeSpace);
+            Console.WriteLine(di.VolumeLabel);
+
+            // Get the root directory and print out some information about it.
+            System.IO.DirectoryInfo dirInfo = di.RootDirectory;
+            Console.WriteLine(dirInfo.Attributes.ToString());
+
+            // Get the files in the directory and print out some information about them.
+            System.IO.FileInfo[] fileNames = dirInfo.GetFiles("*.*");
+
+            foreach (System.IO.FileInfo fi in fileNames)
             {
-                Console.WriteLine(item.Name);
-                MethodInfo[] mi = item.GetMethods();
-
-                foreach (var method in mi)
-                {
-                    Console.WriteLine(method.Name);
-
-                }
+                Console.WriteLine("{0}: {1}: {2}", fi.Name, fi.LastAccessTime, fi.Length);
             }
-            Console.ReadLine();
+
+            // Get the subdirectories directly that is under the root.
+            System.IO.DirectoryInfo[] dirInfos = dirInfo.GetDirectories("*.*");
+
+            foreach (System.IO.DirectoryInfo d in dirInfos)
+            {
+                Console.WriteLine(d.Name);
+            }
+
+            // Get the current application directory.
+            string currentDirName = System.IO.Directory.GetCurrentDirectory();
+            Console.WriteLine(currentDirName);
+            // Get an array of file names as strings rather than FileInfo objects.
+            // Use this method when storage space is an issue, and when you might
+            // hold on to the file name reference for a while before you try to access
+            // the file.
+            string[] files = System.IO.Directory.GetFiles(currentDirName, "*.txt");
+
+            foreach (string s in files)
+            {
+                // Create the FileInfo object only when needed to ensure
+                // the information is as current as possible.
+                System.IO.FileInfo fi = null;
+                try
+                {
+                    fi = new System.IO.FileInfo(s);
+                }
+                catch (System.IO.FileNotFoundException e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+                Console.WriteLine("{0} : {1}", fi.Name, fi.Directory);
+            }
+            Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
         }
     }
 }
