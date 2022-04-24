243fcd8927da993f2e706fef560ff7637d4c8bcd
apekshaalaad <102643138+apekshaalaad@users.noreply.github.com>
2022-04-24 20:46:52 +0530

    Add files via upload

---
+++ CsharpAssign6/Account.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpAssign6
+{
+    public delegate void delegatemethod(int a);
+    internal class Account
+    {
+        public int AccountNumber { get; set; }
+
+        public string CustomerName { get; set; }
+        public int Balance = 1000;
+        public event delegatemethod underbalance;
+        public event delegatemethod zerobalance;
+        public void getdetails()
+        {
+            Console.WriteLine("Enter Account Number: ");
+            AccountNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Customer Name: ");
+            CustomerName = Console.ReadLine();
+        }
+        public void display()
+        {
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Account Number is: " + AccountNumber);
+            Console.WriteLine("Customer name is: " + CustomerName);
+        }
+        public void getempdetails()
+        {
+            getdetails();
+            display();
+
+        }
+        public void insufficient(int a)
+        {
+            underbalance(a);
+        }
+        public void depositmoney(int b)
+        {
+            zerobalance(b);
+        }
+        public void withdraw(int a)
+        {
+            if (a < Balance && Balance != 0)
+            {
+                Console.WriteLine("Transaction Successfull");
+                Console.WriteLine("Remaining Balance is " + (Balance - a));
+            }
+            else if (a > Balance && Balance != 0)
+            {
+                Console.WriteLine("Insufficient Amount");
+                Console.WriteLine("Your Current Balance is " + Balance + "only");
+            }
+            else
+            {
+                Console.WriteLine("Zero Balance :" + Balance);
+            }
+        }
+        public void Deposit(int a)
+        {
+            Console.Write("Balance After Depositing: " + (Balance + a));
+        }
+        static void Main(string[] args)
+        {
+            Account obj = new Account();
+            obj.getempdetails();
+            Console.WriteLine("Withdraw Amount or Deposit : W or D");
+            string W = Console.ReadLine();
+            if (W == "W")
+            {
+                Console.WriteLine("Enter Amount to Withdraw: ");
+                int Wbalance = Convert.ToInt32(Console.ReadLine());
+                obj.underbalance += new delegatemethod(obj.withdraw);
+                obj.insufficient(Wbalance);
+            }
+            else
+            {
+                Console.WriteLine("Enter Amount to Deposit: ");
+                int Dbalance = Convert.ToInt32(Console.ReadLine());
+                obj.zerobalance += new delegatemethod(obj.Deposit);
+                obj.depositmoney(Dbalance);
+            }
+            Console.Write("\nPress any key to close ");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CsharpAssign6/Employee.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpAssign6
+{
+    public delegate void Delagate();
+    internal class Employee
+    {
+        private int EmpNo;
+        private string EmpName;
+        private double Salary;
+        private double HRA;
+        private double TA;
+        private double DA;
+        private double PF;
+        private double TDS;
+        private double NetSalary;
+        private double GrossSalary;
+        public Employee(int EmpNum, string EmpNm, double Sal)
+        {
+            EmpNo = EmpNum;
+            EmpName = EmpNm;
+            Salary = Sal;
+            if (Salary < 5000)
+            {
+                HRA = Salary * 10 / 100;
+                TA = Salary * 5 / 100;
+                DA = Salary * 15 / 100;
+                GrossSalary = Salary + HRA + TA + DA;
+            }
+            else if (Salary < 10000)
+            {
+                HRA = Salary * 15 / 100;
+                TA = Salary * 10 / 100;
+                DA = Salary * 20 / 100;
+                GrossSalary = Salary + HRA + TA + DA;
+            }
+            else if (Salary < 15000)
+            {
+                HRA = Salary * 20 / 100;
+                TA = Salary * 15 / 100;
+                DA = Salary * 25 / 100;
+                GrossSalary = Salary + HRA + TA + DA;
+            }
+            else if (Salary < 20000)
+            {
+                HRA = Salary * 25 / 100;
+                TA = Salary * 20 / 100;
+                DA = Salary * 30 / 100;
+                GrossSalary = Salary + HRA + TA + DA;
+            }
+            else
+            {
+                HRA = Salary * 30 / 100;
+                TA = Salary * 25 / 100;
+                DA = Salary * 35 / 100;
+                GrossSalary = Salary + HRA + TA + DA;
+            }
+
+        }
+        public virtual void Calculatesalary()
+        {
+            PF = GrossSalary * 10 / 100;
+            TDS = GrossSalary * 18 / 100;
+            NetSalary = GrossSalary - (PF * TDS);
+        }
+        public virtual void Gsal()
+        {
+            Console.WriteLine(GrossSalary);
+        }
+        class Manager : Employee
+        {
+            private double petrol { get; set; }
+            private double food { get; set; }
+            private double other { get; set; }
+            public Manager(int number, string name, double sal) : base(number, name, sal)
+            {
+                petrol = Salary * 8 / 100;
+                food = Salary * 13 / 100;
+                other = Salary * 3 / 100;
+            }
+            public override void Gsal()
+            {
+                GrossSalary = (GrossSalary + petrol + food + other);
+                Console.WriteLine(GrossSalary);
+            }
+            public override void Calculatesalary()
+            {
+                PF = GrossSalary * 10 / 100;
+                TDS = GrossSalary * 18 / 100;
+                NetSalary = GrossSalary - (PF + TDS);
+                Console.WriteLine(NetSalary);
+            }
+        }
+        class Marketingexecutive : Employee
+        {
+            private double kilometertravel { get; set; }
+            private double tourallowance { get; set; }
+            private double telephoneallowance { get; set; }
+
+            public Marketingexecutive(int number, string name, double sal, double kit) : base(number, name, sal)
+            {
+                kilometertravel = kit;
+                tourallowance = kilometertravel * 5;
+                telephoneallowance = 1000;
+            }
+            public override void Gsal()
+            {
+                GrossSalary = (GrossSalary + tourallowance + telephoneallowance);
+                Console.WriteLine(GrossSalary);
+            }
+            public override void Calculatesalary()
+            {
+                PF = GrossSalary * 10 / 100;
+                TDS = GrossSalary * 18 / 100;
+                NetSalary = GrossSalary - (PF + TDS);
+                Console.WriteLine(NetSalary);
+            }
+           
+        }
+        class MainProgram
+        {
+            static void Main(string[] args)
+            {
+                Manager M1 = new Manager(1, "Madhavi", 10000);
+                Delagate MD = new Delagate(M1.Gsal);
+                MD += new Delagate(M1.Calculatesalary);
+                MD();
+
+                Marketingexecutive M2 = new Marketingexecutive(1, "Nidhi", 10000,45);
+                Delagate MED = new Delagate(M2.Gsal);
+                MED += new Delagate(M2.Calculatesalary);
+                MED();
+                /*Manager MarkE1 = new Manager(2, "Apeksha", 15000);
+                MarkE1.Gsal();
+
+
+                Marketingexecutive MarkE2 = new Marketingexecutive(3, "Apeksha", 7000, 45);
+                MarkE2.Gsal();*/
+
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
